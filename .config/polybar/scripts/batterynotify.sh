9caa40471d6c6de7ab956fa083ad48daa4c0af84
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2020-11-27 16:58:10 +0530

    Clean up battery notify to use acpi instead of upower

--- .config/polybar/scripts/batterynotify.sh
+++ .config/polybar/scripts/batterynotify.sh
@@ -1,17 +1,5 @@
 #! /usr/bin/env sh
 
-state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep "state" | awk '{print $2}')
+state=$(acpi --battery | grep -v Unknown | awk '{print $3 " "  $4 " "  $5}' | sed 's/,//')
 
-case $state in
-	discharging)
-		timemsg=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep "time to empty" | awk '{print $4 " " $5}')
-		;;
-
-	charging)
-		timemsg=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep "time to full" | awk '{print $4 " " $5}')
-		;;
-esac
-
-notify-send -a "system" -u LOW \
-	"Battery $(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage | awk '{print $2}')
-	${state^} - $timemsg"
+notify-send -a "system" -u LOW "$(echo -e Battery\\n$state)"
