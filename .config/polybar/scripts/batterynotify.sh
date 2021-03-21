030ff63e523ffdb3933d01a101de1a393b2740b7
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2021-03-21 15:25:42 +0530

    Update battery notif to ignore more invalid batteries

--- .config/polybar/scripts/batterynotify.sh
+++ .config/polybar/scripts/batterynotify.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
 
-state=$(acpi --battery | grep -v Unknown | awk '{print $3 " "  $4 " "  $5}' | sed 's/,//')
+state=$(acpi --battery | grep -v "Unknown\|unavailable" | awk '{print $3 " "  $4 " "  $5}' | sed 's/,//')
 
 notify-send -a "system" -u LOW "$(echo -e Battery\\n$state)"
