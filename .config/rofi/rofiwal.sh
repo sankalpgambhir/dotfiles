e4e73e8a0a0d81816eec8520eddeaf390e1ec32d
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2021-10-11 16:22:02 +0530

    Updates to auto color updates due to breaking software changes

--- .config/rofi/rofiwal.sh
+++ .config/rofi/rofiwal.sh
@@ -5,7 +5,5 @@ source "$HOME/.cache/wal/colors.sh"
 walargs=(-padding 30
 	-eh 1.7
      	-font "mononoki Nerd Font 14"
-     	-color-window "$background, $background, $color1"
-     	-color-normal "$background, $foreground, $background, $color1, $background"
-     	-color-active "$background, $foreground, $background, $color1, $background"
+     	-theme "$HOME/.cache/wal/colors-rofi-light.rasi"
 )
